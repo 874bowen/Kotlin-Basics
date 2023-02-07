7adbea1fa689059b97c18fa0148cab5d1ddf73e6
Ivan Bowen <bowenivan16@gmail.com>
2023-02-07 14:23:49 +0300

    strings in Kotlin

---
+++ Strings/strings.kt
@@ -0,0 +1,14 @@
+fun main(){
+   val string = "I am a programmer"
+   println(string.uppercase())
+   val x = 1 + 1
+   if (x == 2){
+      println("x is 2")
+   } else if (x == 3){
+      println("x is 3")
+   } else {
+      println("x is not 2 or 3")
+   }
+   val y = if (x == 5) 4 else 858
+   println(y)
+}
