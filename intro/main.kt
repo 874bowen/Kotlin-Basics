aad03d8f490105be3065234035414eff28fa9d0d
Ivan Bowen <bowenivan16@gmail.com>
2023-02-07 14:04:21 +0300

    variables and arithmetic operators in kotlin

---
+++ intro/main.kt
@@ -0,0 +1,23 @@
+fun main() {
+   // val cannot be assigned
+   val x: Int = 3
+   val z = 3.44f
+   var y: Int = 34
+   val wholeNumber = 3
+   val bigNumber = 3L
+   val preciseDecimal = 3.33
+   val decimcal = 3.33f
+   val iLoveVeges = false
+   val iLoveCats = false
+   val sentence = "I am Ivan Bowen"
+   // division will return 0 because its not floating numbers
+   // var mul = 3 / 4
+   // to get a floating result add f
+   var mul = 3f / 4
+   var modulo = 10 % 6
+   println("The val of mul is $mul")
+   println("The val of modulo is $modulo")
+   println("The value of x is $x and y is $y")
+   y = 4
+   println("The new value of y is $y")
+}
